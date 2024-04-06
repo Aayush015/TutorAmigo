9e912b07ad98f6c4cd389d2f01e45d12e03e5758
Aayush015 <aayushkafle308@gmail.com>
2024-04-05 21:23:35 -0600

    Added PHP for user validation

---
+++ php/includes/connect.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration
+$host = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "registrationform_db";
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+?>
